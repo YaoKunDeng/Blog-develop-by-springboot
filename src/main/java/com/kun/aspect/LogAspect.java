f4b39799a4f339056b41244e93567fd0f78a9dd8
YaoKunDeng <YaoKunDeng@163.com>
2022-05-15 22:50:06 +0800

    日志处理

---
+++ src/main/java/com/kun/aspect/LogAspect.java
@@ -0,0 +1,87 @@
+package com.kun.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import sun.rmi.runtime.Log;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
+
+//1. 记录日志内容【采用aop方式】
+//        1. 请求rul
+//        2. 访问者ip
+//        3. 调用方法
+//        4. 参数args
+//        5. 返回内容
+
+
+
+
+@Aspect
+@Component
+public class LogAspect {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Pointcut("execution(* com.kun.web.*.*(..))")
+    public void log(){
+
+    }
+
+    @Before("log()")   //表示这个方法在切面之前执行
+    public void doBefore(JoinPoint joinPoint){ //JoinPoint切面对象
+        //拿到request,用request去获取需要的东西
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURL().toString();
+        String ip = request.getRemoteAddr();
+        String classMethod = joinPoint.getSignature().getDeclaringTypeName() + "." +joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+        RequestLog requestLog = new RequestLog(url ,ip, classMethod, args);
+        logger.info("Request:{}",requestLog);
+
+    }
+    @Before("log()")
+    public void doAfter(){
+//        logger.info("--------------Aftere--------------");
+    }
+
+    @AfterReturning(returning = "result",pointcut = "log()")
+    public void doAfterReturn(Object result){
+        logger.info("Result:{}",result);
+    }
+
+//    内部类，目的是将需要用到的参数封装
+    private class RequestLog{
+        private String url;  //请求的rul
+        private String ip;  //访问者ip
+        private String className;   //调用方法
+        private Object[] args;  //参数args
+
+    public RequestLog(String url, String ip, String className, Object[] args) {
+        this.url = url;
+        this.ip = ip;
+        this.className = className;
+        this.args = args;
+    }
+
+    @Override
+    public String toString() {
+        return "RequestLog{" +
+                "url='" + url + '\'' +
+                ", ip='" + ip + '\'' +
+                ", className='" + className + '\'' +
+                ", args=" + Arrays.toString(args) +
+                '}';
+    }
+}
+}
+
+
--- src/main/java/com/kun/web/IndexController.java
+++ src/main/java/com/kun/web/IndexController.java
@@ -3,7 +3,11 @@ package com.kun.web;
 
 import com.kun.NotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Arrays;
 
 /*
 * Created by Daniel on 2022/5/15
@@ -11,13 +15,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,@PathVariable String name){
 //        int i=9/0;
-        String blog = null;
-        if(blog == null){
-            throw  new NotFoundException("博客不存在");
-        }
+//        String blog = null;
+//        if(blog == null){
+//            throw  new NotFoundException("博客不存在");
+//        }
+        System.out.println("---------index------------");
         return "index";
     }
+
+
 }
